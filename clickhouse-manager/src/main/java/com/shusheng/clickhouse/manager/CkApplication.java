90d677757911dd9357405d4205655582a0e52058
shusheng0102 <shusheng0102@126.com>
2023-04-13 06:58:29 +0800

    add ck

---
+++ clickhouse-manager/src/main/java/com/shusheng/clickhouse/manager/CkApplication.java
@@ -0,0 +1,11 @@
+package com.shusheng.clickhouse.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CkApplication.class,args);
+    }
+}
